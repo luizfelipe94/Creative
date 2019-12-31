c85698e52080351c53e42f6e66b8b76175245142
Luiz Felipe da Silva <luizfelipe.dasilva94@gmail.com>
2019-12-31 00:03:18 -0300

    ajustando formulário

--- src/components/FormBoard.js
+++ src/components/FormBoard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { withStyles } from "@material-ui/core/styles";
-
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import Question from "./Question";
 
 const questions = [
@@ -31,22 +36,69 @@ const questions = [
     },
 ];
 
-const styles = {
-    root: {}
-};
+const useStyles = makeStyles(theme => ({
+    root: {
+        maxWidth: 400,
+        flexGrow: 1,
+    },
+    header: {
+        display: 'flex',
+        alignItems: 'center',
+        height: 50,
+        paddingLeft: theme.spacing(4),
+        backgroundColor: theme.palette.background.default,
+    },
+    img: {
+        height: 255,
+        maxWidth: 400,
+        overflow: 'hidden',
+        display: 'block',
+        width: '100%',
+    },
+}));
 
-const FormBoard = ({ children, classes }) => (
-    <Container className={classes.root} alignItems="center" justify="center">
-        {questions.map(q => <Question question={q} />)}
-    </Container>
-)
 
-// class FormBoard extends React.Component {
+const FormBoard = () => {
 
-//     constructor(props){
-//         super(props);
-//         this.questions = questions;
-//     }
-// }
+    const classes = useStyles();
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = questions.length;
 
-export default withStyles(styles)(FormBoard);
+    const handleNext = () => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    };
+
+    return (
+        <Container className={classes.root}>
+            <Paper square elevation={0} className={classes.header}>
+                <Typography>{questions[activeStep].label}</Typography>
+            </Paper>
+            <Question question={questions[activeStep]} />
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                variant="text"
+                activeStep={activeStep}
+                nextButton={
+                    <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+                        Next
+                        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                        Back
+                    </Button>
+                }
+            />
+        </Container>
+    );
+}
+
+export default FormBoard;
